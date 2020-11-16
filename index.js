503244572cd64522c0605d27048e064e7a42aef7
sky192 <samahehloghmani@gmail.com>
2020-11-15 22:46:01 -0500

    completed movies.js file

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.get('/movies/:search', searchMovieByTitleOrDirector)
 app.post('/movies', bodyParser.json(), saveNewMovie)
 
 app.listen(1337, () => {
-    console.log('Listening on port 1337...')
+  console.log('Listening on port 1337...') // eslint-disable-line no-console
+})
